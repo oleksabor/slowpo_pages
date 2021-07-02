ff172bd34e90c752985af27653b3108d3cf078cd
oleksii borodai <oleksii.borodai@quipu.de>
2021-07-02 19:14:39 +0300

    initial

---
+++ _posts/2021-05-27-short-syntax-for-WPF-DependencyProperty-registration.md
@@ -0,0 +1,18 @@
+---
+layout: post
+title: short syntax for WPF DependencyProperty registration
+date: 2021-05-27 00:00 
+category: dev
+---
+
+Would like to share short syntax for depencecy properties registration found on web
+
+{% highlight csharp %}
+public static readonly DependencyProperty TypeNameProperty = RegisterProperty(_ => _.TypeName).OnChange(_ => _.OnTypeNameChanged);
+
+public static readonly DependencyProperty BankClientProperty = RegisterProperty(_ => _.Client).Default(null);
+{% endhighlight %}
+
+Found at the https://github.com/dotnet/wpf/issues/2352
+
+`DependencyPropertyRegistrar` source code is stored at the https://gist.github.com/zlatanov/8ed191d517d2a84529f66b73347cd7ca
---
+++ newPost.ps1
@@ -0,0 +1,65 @@
+param([string]$title, [string]$tags, [string]$category, [string]$dateO)
+$date = Get-Date
+
+if ($dateO -ne "") {
+	[System.DateTime]::TryParse($dateO, [ref]$date)
+}
+
+$now = $date.ToString("yyyy-MM-dd HH:mm K");
+
+function transliterate([string] $text){
+	$s = @( 'а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я'
+	,'А', 'Б', 'В', 'Г', 'Ґ', 'Д', 'Е', 'Є', 'Ж', 'З', 'И', 'І', 'Ї', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ю', 'Я'
+	, "`'")
+	
+	$d = @( 'a', 'b', 'v', 'g', 'g', 'd', 'e', 'e', 'zh', 'z', 'y', 'i', 'i', 'y', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'ts', 'ch', 'sh', 'shc', '', 'yu', 'ya'
+	,
+	'A', 'B', 'V', 'G', 'G', 'D', 'E', 'E', 'Zh', 'Z', 'Y', 'I', 'I', 'Y', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'Ts', 'Ch', 'Sh', 'Shc', '', 'Yu', 'Ya'
+	, '');
+	
+	for ($i = 0; $i -lt $s.Length; $i++)
+	{
+		if ($text.Contains($s[$i])) {
+			$text = $text.Replace($s[$i], $d[$i])
+		}
+	}
+	$a = '';
+	return $text
+}
+
+$content  = @()
+$content  += "---"
+$content  += "layout: post"
+$content  += "title: $title"
+$content  += "date: $now" 
+
+
+if ($tags -ne "") {
+	$content += "tags: [$tags]"
+}
+if ($category -ne "") {
+	$content += "category: $category"
+}
+$content  += "---"
+
+
+
+$content -join "`n";
+
+$titleTR = transliterate($title);
+
+$titlewords = $titleTR.Split(" ") | where {$_.Length -gt 1};
+
+$fn = -join ("_posts/", $date.ToString("yyyy-MM-dd"), "-");
+
+$fn += $titlewords -join "-" ;
+$fn += ".md";
+
+$fn
+
+$content | Out-File -FilePath $fn
+
+& "notepad++" $fn #open in notepad to edit
+
+
+
