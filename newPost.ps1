555e7020c1b0ed865bd1ea273e816ef3253a12f7
oleksii borodai <oleksa.borodie@gmail.com>
2021-07-11 13:59:11 +0300

    non ascii chars removed from file name

--- newPost.ps1
+++ newPost.ps1
@@ -1,4 +1,4 @@
-param([string]$title, [string]$tags, [string]$category, [string]$dateO)
+param([string]$title, [string]$tags, [string]$category, [string]$dateO, [switch]$debug)
 $date = Get-Date
 
 if ($dateO -ne "") {
@@ -13,13 +13,13 @@ function transliterate([string] $text){
 	$s = @( 'а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я'
 	,'А', 'Б', 'В', 'Г', 'Ґ', 'Д', 'Е', 'Є', 'Ж', 'З', 'И', 'І', 'Ї', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ю', 'Я'
 	, "`'"
-	, 'ș')
+	, 'ș', '<', '>', '(', ')', '[', ']')
 	
 	$d = @( 'a', 'b', 'v', 'g', 'g', 'd', 'e', 'e', 'zh', 'z', 'y', 'i', 'i', 'y', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'ts', 'ch', 'sh', 'shc', '', 'yu', 'ya'
 	,
 	'A', 'B', 'V', 'G', 'G', 'D', 'E', 'E', 'Zh', 'Z', 'Y', 'I', 'I', 'Y', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'Ts', 'Ch', 'Sh', 'Shc', '', 'Yu', 'Ya'
 	, ''
-	, 'sh');
+	, 'sh', '', '', '', '', '', '');
 	
 	for ($i = 0; $i -lt $s.Length; $i++)
 	{
@@ -56,6 +56,8 @@ $content -join "`n";
 
 $titleTR = transliterate($title);
 
+$titleTR = $titleTR -Replace '[^a-zA-Z0-9\-\s]', ''
+
 $titlewords = $titleTR.Split(" ") | where {$_.Length -gt 1};
 
 $fn = -join ("_posts/", $date.ToString("yyyy-MM-dd"), "-");
@@ -65,9 +67,10 @@ $fn += ".md";
 
 $fn
 
+if (-not $Debug) {
 $content | Out-File -FilePath $fn
 
 & "notepad++" $fn #open in notepad to edit
-
+}
 
 
