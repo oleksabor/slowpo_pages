05c0e23e0bc83a5a89482bed3b02c13b73153b92
oleksii borodai <oleksa.borodie@gmail.com>
2021-10-09 19:37:20 +0300

    transliteration logging

--- newPost.ps1
+++ newPost.ps1
@@ -16,6 +16,8 @@ if ($dateO -ne "") {
 $now = $date.ToString("yyyy-MM-dd HH:mm K");
 
 function transliterate([string] $text){
+	Write-Host "transliterating [$text]";
+
 	$s = @( 'а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я'
 	,'А', 'Б', 'В', 'Г', 'Ґ', 'Д', 'Е', 'Є', 'Ж', 'З', 'И', 'І', 'Ї', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ю', 'Я'
 	, "`'"
@@ -34,7 +36,8 @@ function transliterate([string] $text){
 		}
 	}
 	$a = '';
-	return $text -Replace '[^\w\s-]', ''
+	Write-Host "transliterated to [$text]";
+	return $text -Replace '[^\w\s\d-]', ''
 }
 
 $title = $title -replace '\:', ''; # no colon allowed in title caption
